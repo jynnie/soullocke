81523834bd54a2235970e896013dfe785f2cc99a
jynnie <jynnie@mit.edu>
2021-01-02 14:37:46 -0800

    :fire: Switching from api to context

---
+++ pages/_app.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Head from "next/head";
+import firebase from "firebase";
+import firebaseConfig from "firebaseConfig";
+
+import "../styles/globals.css";
+
+//- Firebase Setup
+let db;
+try {
+  firebase.initializeApp(firebaseConfig);
+  db = firebase.database();
+} catch (error) {
+  /*
+   * We skip the "already exists" message which is
+   * not an actual error when we're hot-reloading.
+   */
+  if (!/already exists/u.test(error.message)) {
+    // eslint-disable-next-line no-console
+    console.error("Firebase admin initialization error", error.stack);
+  }
+}
+export const FirebaseContext = React.createContext(null);
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-analytics.js"></script>
+      </Head>
+      <FirebaseContext.Provider value={{ db }}>
+        <Component {...pageProps} />
+      </FirebaseContext.Provider>
+    </>
+  );
+}
+
+export default MyApp;
--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import db from "lib/firebase";
-
-export default (req, res) => {
-  res.statusCode = 200;
-  res.end("hello");
-  // res.json({ name: "John Doe" });
-};
