2705fe7e1bf36faf48210701da37d669340867e6
jynnie <jynnie@mit.edu>
2021-01-05 23:26:02 -0800

    🔥 Adding the firebase config

---
+++ firebaseConfig.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBY38WzK0UmMfq4lvPyRCK33NKEZfWlPro",
+  authDomain: "soullocke-f7500.firebaseapp.com",
+  databaseURL: "https://soullocke-f7500-default-rtdb.firebaseio.com",
+  projectId: "soullocke-f7500",
+  storageBucket: "soullocke-f7500.appspot.com",
+  messagingSenderId: "928329131907",
+  appId: "1:928329131907:web:9ad30e6856fd586566b8b6",
+  measurementId: "G-NKZJHF6JTY",
+};
+
+export default firebaseConfig;
