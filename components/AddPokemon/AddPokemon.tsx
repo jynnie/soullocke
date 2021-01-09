ea195eb220760a9bdf554dde8745cb6afa286bd1
jynnie <jynnie@mit.edu>
2021-01-09 01:41:52 -0800

    🔁 Prompt to swap in Pokemon to your team after inputting
    - Additionally am extra setting origin for pokemon in soul-linked events

--- components/AddPokemon/AddPokemon.tsx
+++ components/AddPokemon/AddPokemon.tsx
@@ -10,21 +10,24 @@ import Form from "./Form";
 export function AddPokemon({
   playerId,
   location,
+  onFinish,
 }: {
   playerId: string;
   location: PlaceName;
+  onFinish: (caught: boolean) => void;
 }) {
   const { RUN } = React.useContext(RunContext);
   const [showForm, setShowForm]: UseState<boolean> = React.useState(null);
 
   const toggleForm = () => setShowForm(!showForm);
 
-  const onFinish = async (
+  const handleFinish = async (
     pokemonName: string,
     nickname: string,
     caught: boolean,
   ) => {
     await RUN.addPokemon(pokemonName, nickname, playerId, location, caught);
+    if (onFinish) onFinish(caught);
     toggleForm();
   };
   const onCancel = () => {
@@ -33,7 +36,7 @@ export function AddPokemon({
 
   return (
     <Popover
-      content={<Form {...{ onFinish, onCancel }} />}
+      content={<Form {...{ onFinish: handleFinish, onCancel }} />}
       trigger="click"
       visible={showForm}
       onVisibleChange={(v) => setShowForm(v)}
--- components/MovePokemonToTeam/MovePokemonToTeam.tsx
+++ components/MovePokemonToTeam/MovePokemonToTeam.tsx
@@ -1,10 +1,117 @@
 import React from "react";
+import { isNullish, oKey, oVal } from "lib/utils";
+import { RunContext } from "pages/run/[id]";
+
+import styles from "styles/Pokemon.module.scss";
+import PokemonGroup from "components/PokemonGroup";
+import PokemonIcon from "components/PokemonIcon";
+import { Button, Modal } from "antd";
+import { Pokemon, PlaceName, UseState } from "models";
 
 /**
  * Prompt for Moving Pokemon to Team
- * If team is full, will also ask for
- * switches.
+ * If team is full, will also ask for switches.
  */
-export function MovePokemonToTeam() {}
+export function MovePokemonToTeam({
+  pokemonOrigin,
+  currentLocation,
+  visible,
+  onCancel,
+}: {
+  pokemonOrigin: PlaceName;
+  currentLocation: PlaceName;
+  visible: boolean;
+  onCancel?: () => void;
+}) {
+  const { RUN } = React.useContext(RunContext);
+
+  // Information Getters
+  const teamsByPlayer = RUN.getPokemonOnTeam();
+  const teamLength = oVal(teamsByPlayer)[0]?.length;
+  const noSwapsNeeded = teamLength < 6;
+  const numSwapsNeeded = teamLength - 5;
+  const pokemonToSwitchIn = RUN.getPokemonByOrigin(pokemonOrigin);
+  const pokemonToSwitchNames = pokemonToSwitchIn
+    ?.map((p) => p?.nickname || "?")
+    .join(" & ");
+  const teamArr: Pokemon[] = oVal(teamsByPlayer).reduce(
+    (acc, ps) => acc.concat(...ps),
+    [],
+  );
+  const teamGroupedByOrigin = RUN.groupByOrigin(teamArr);
+
+  // Handlers for actually swapping
+  const [swapOut, setSwapOut]: UseState<PlaceName[]> = React.useState([]);
+  const selectGroup = (origin: PlaceName) => {
+    let newVal = [...swapOut];
+    newVal.push(origin);
+    newVal = newVal.slice(-numSwapsNeeded);
+    setSwapOut(newVal);
+  };
+  const handleCancel = () => {
+    setSwapOut([]);
+    if (onCancel) onCancel();
+  };
+  const handleAdd = async () => {
+    await RUN.swapPokemonOnTeam(pokemonOrigin, swapOut, currentLocation);
+    if (onCancel) return onCancel();
+    return true;
+  };
+
+  // Loading stuff
+  const loadingRunData = isNullish(teamLength);
+  if (loadingRunData) return null;
+
+  const footerButtons = [
+    <Button key="cancel" onClick={handleCancel}>
+      Cancel
+    </Button>,
+    <Button
+      key="add"
+      disabled={!noSwapsNeeded && swapOut.length !== numSwapsNeeded}
+      onClick={handleAdd}
+      type="primary"
+    >
+      {noSwapsNeeded ? "Add" : "Swap"}
+    </Button>,
+  ];
+
+  return (
+    <Modal
+      title={`Add ${pokemonToSwitchNames} to your teams?`}
+      footer={footerButtons}
+      {...{ visible, onCancel }}
+    >
+      <div className="flex column center">
+        <PokemonGroup marginBottom={12}>
+          {pokemonToSwitchIn.map((p, i) => (
+            <PokemonIcon key={i} pokemon={p} />
+          ))}
+        </PokemonGroup>
+
+        {noSwapsNeeded && <p>No swaps necessary to add</p>}
+        {!noSwapsNeeded && (
+          <p>Need to swap out {numSwapsNeeded} pair(s) of Pokemon</p>
+        )}
+        {!noSwapsNeeded && (
+          <div className={styles.party}>
+            {oKey(teamGroupedByOrigin).map((o, i) => (
+              <PokemonGroup
+                actionable
+                key={i}
+                onClick={() => selectGroup(o)}
+                selected={swapOut.includes(o)}
+              >
+                {teamGroupedByOrigin[o].map((p, j) => (
+                  <PokemonIcon hideBadge key={j} pokemon={p} />
+                ))}
+              </PokemonGroup>
+            ))}
+          </div>
+        )}
+      </div>
+    </Modal>
+  );
+}
 
 export default MovePokemonToTeam;
