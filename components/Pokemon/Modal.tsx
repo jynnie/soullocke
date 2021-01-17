1fdf93dd872770720f9f3b0f7b7cd1d05966467a
jynnie <jynnie@mit.edu>
2021-01-16 16:03:39 -0800

    🍬 Edit Pokemon name and nickname
    Helps in case of mistakes

--- components/Pokemon/Modal.tsx
+++ components/Pokemon/Modal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cleanName, oVal } from "lib/utils";
 import { RunContext } from "pages/run/[id]";
-import { Pokemon, PokemonLocation } from "models";
+import { Pokemon, PokemonLocation, UseState } from "models";
 
 import Event from "./Event";
 import AddEvent from "components/AddEvent";
@@ -9,7 +9,8 @@ import PokemonImage from "components/PokemonImage";
 import PokemonForm from "components/AddPokemon/Form";
 import PLTag from "components/LocationSummary/PLTag";
 import styles from "styles/Pokemon.module.scss";
-import { Modal, Timeline } from "antd";
+import { Button, Modal, Timeline } from "antd";
+import { EditOutlined } from "@ant-design/icons";
 
 function PokemonModal({
   pokemon,
@@ -25,6 +26,7 @@ function PokemonModal({
   onCancel: () => void;
 }) {
   const { RUN } = React.useContext(RunContext);
+  const [editPokemon, setEditPokemon]: UseState<boolean> = React.useState(null);
 
   const timelineArr = RUN.getTimelineLocationNames();
   const eventsArr = oVal(pokemon?.events || {}).sort(
@@ -32,26 +34,51 @@ function PokemonModal({
   );
   const pokemonIsAlive = pokemon.location !== PokemonLocation.grave;
 
+  const handleCancelEditing = () => {
+    setEditPokemon(false);
+  };
+
   const handleFinish = (pokemonName: string, nickname: string): void => {
     RUN.backfillPokemon(pokemonName, nickname, playerId, location);
+    if (editPokemon) setEditPokemon(false);
   };
 
   return (
     <Modal visible={!!showModal} onCancel={onCancel} footer={null}>
-      <div className={styles.modalHeader}>
-        <PokemonImage className={styles.modalHeaderImg} pokemon={pokemon} />
-        <h3>{cleanName(pokemon?.nickname)}</h3>
-        <div className="flex center">
-          <PLTag
-            className={styles.modalHeaderTag}
-            pokemonLocation={pokemon?.location}
-          />
-          <h4>{cleanName(pokemon?.name)}</h4>
-        </div>
-      </div>
+      {!editPokemon && (
+        <div className={styles.modalHeader}>
+          <PokemonImage className={styles.modalHeaderImg} pokemon={pokemon} />
+          <h3>{cleanName(pokemon?.nickname)}</h3>
+          <div className="flex center">
+            <PLTag
+              className={styles.modalHeaderTag}
+              pokemonLocation={pokemon?.location}
+            />
+            <h4>{cleanName(pokemon?.name)}</h4>
+          </div>
 
-      {!pokemon.name && (
-        <PokemonForm showCaughtCheckbox={false} onFinish={handleFinish} />
+          <Button
+            className={styles.modalHeaderEdit}
+            type="primary"
+            shape="circle"
+            onClick={() => setEditPokemon(!editPokemon)}
+          >
+            <EditOutlined />
+          </Button>
+        </div>
+      )}
+
+      {(!pokemon.name || editPokemon) && (
+        <PokemonForm
+          showCaughtCheckbox={false}
+          onCancel={editPokemon && handleCancelEditing}
+          onFinish={handleFinish}
+          finishText={editPokemon ? "Save" : "Add"}
+          defaultValues={{
+            pokemon: pokemon?.name,
+            nickname: pokemon?.nickname,
+          }}
+        />
       )}
 
       <Timeline>
