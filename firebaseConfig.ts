7ef5b22f2d5baa2746a6c7eec5e826bffa67c9d2
jynnie <jynnie@mit.edu>
2021-10-03 02:00:47 -0700

    🔥 Adding the firebase config

---
+++ firebaseConfig.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: "G-NKZJHF6JTY",
+};
+
+export default firebaseConfig;
