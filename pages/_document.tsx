f1513198d44cd94aba7089b4fa4c0828c6e492fb
jynnie <jynnie@mit.edu>
2021-11-26 12:59:23 -0800

    🔖 Add SEO tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,24 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <meta
+            name="og:title"
+            property="og:title"
+            content="Soullocke: Pokémon Soul-link Nuzlocke Tracker"
+          />
+          <meta property="og:url" content="https://soullocke.vercel.app/" />
+          <meta
+            name="og:description"
+            content="Soullocke (or soul-linked Nuzlocke) Tracker for encounters and progress. All Pokémon games (Sapphire, Emerald, Diamond, Pearl, Black, White, Sword, Shield, etc). Works best for two trainers, but can support any number in a run."
+          />
+          <meta property="og:image" content="/clip.png" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:creator" content="@jynniit" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
         <body>
           <Main />
           <NextScript />
