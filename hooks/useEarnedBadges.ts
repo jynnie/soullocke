ec31cfe96aecd7795fa652babf9f5c8b59d842db
jynnie <jynn.tang@gmail.com>
2022-09-12 21:39:28 -0700

    🥳 Fix badges

--- hooks/useEarnedBadges.ts
+++ hooks/useEarnedBadges.ts
@@ -2,6 +2,8 @@ import { useTimelineLocations } from "./useTimelineLocations";
 
 export default function useEarnedBadges() {
   const allLocations = useTimelineLocations();
-  const allBadges = allLocations.filter((l) => /badge/gi.test(l.name));
+  const allBadges = allLocations
+    .filter((l) => /badge/gi.test(l.name))
+    .map((l) => l.name);
   return allBadges;
 }
