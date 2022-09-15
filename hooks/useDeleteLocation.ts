477de4edea4c03062056f0eecf9ffea1073a3bfa
jynnie <jynn.tang@gmail.com>
2022-09-14 21:30:10 -0700

    Fixes #8
    Make deleting a location also delete the Pokémon

--- hooks/useDeleteLocation.ts
+++ hooks/useDeleteLocation.ts
@@ -1,12 +1,26 @@
-import { PlaceName, Timeline } from "models/db.model";
+import { Player, Timeline } from "models/db.model";
 
 import { useRunChild } from "./useRun";
 
-export function useDeleteLocation(location: PlaceName) {
-  const timeline = useRunChild<Timeline>(`timeline/${location}`);
+export function useDeleteLocation(locationKey: string) {
+  const timeline = useRunChild<Timeline>(`timeline/${locationKey}`);
+  const players = useRunChild<Record<string, Player>>("players", {});
 
   async function deleteLocation() {
-    timeline.set(null);
+    const promises: Promise<any>[] = [];
+
+    const promise = timeline.set(null);
+    if (promise) promises.push(promise);
+
+    for (const player of Object.values(players.value || {})) {
+      const promise = players.ref
+        ?.child(`${player.id}/pokemon/${locationKey}`)
+        .set(null);
+
+      if (promise) promises.push(promise);
+    }
+
+    await Promise.all(promises);
   }
 
   return deleteLocation;
