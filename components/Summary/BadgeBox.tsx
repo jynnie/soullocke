319867f38de58c907b5a2c1603cba8fe4efaf36f
jynnie <jynn.tang@gmail.com>
2022-10-17 13:58:01 -0700

    🎖 Toggle badges from summary view

--- components/Summary/BadgeBox.tsx
+++ components/Summary/BadgeBox.tsx
@@ -1,5 +1,9 @@
+import { Tooltip } from "antd";
 import cn from "classnames";
+import { useAddNewLocation } from "hooks/useAddNewLocation";
+import { useDeleteLocation } from "hooks/useDeleteLocation";
 import useEarnedBadges from "hooks/useEarnedBadges";
+import { useTimelineLocations } from "hooks/useTimelineLocations";
 import React from "react";
 import styles from "styles/Summary.module.scss";
 
@@ -14,15 +18,50 @@ export function BadgeBox({ allBadges }: { allBadges: string[] }) {
   return (
     <div className={styles.badgeBox}>
       {allBadges.map((badge) => (
-        <img
+        <SingleBadge
           key={badge}
-          className={cn({ [styles.earned]: earnedBadges.includes(badge) })}
-          src={`https://nuzlocke-generator.herokuapp.com/img/checkpoints/${badge
-            .replaceAll("_", "-")
-            .toLowerCase()}.png`}
-          alt={badge}
+          badge={badge}
+          isEarned={earnedBadges.includes(badge)}
         />
       ))}
     </div>
   );
+}
+
+function SingleBadge({
+  badge,
+  isEarned,
+}: {
+  badge: string;
+  isEarned: boolean;
+}) {
+  const addNewLocation = useAddNewLocation();
+  const badgeKey = useBadgeLocationKey(badge);
+  const deleteLocation = useDeleteLocation(badgeKey ?? "");
+
+  function addBadge(badge: string) {
+    return () => {
+      if (isEarned) badgeKey && deleteLocation();
+      else addNewLocation(badge);
+    };
+  }
+
+  return (
+    <Tooltip key={badge} title="Double click to toggle badge">
+      <img
+        className={cn({ [styles.earned]: isEarned }, "pointer")}
+        src={`https://nuzlocke-generator.herokuapp.com/img/checkpoints/${badge
+          .replaceAll("_", "-")
+          .toLowerCase()}.png`}
+        alt={badge}
+        onDoubleClick={addBadge(badge)}
+      />
+    </Tooltip>
+  );
+}
+
+function useBadgeLocationKey(badge: string) {
+  const allLocations = useTimelineLocations();
+  const badgeKey = allLocations.find((l) => l.name === badge)?.key;
+  return badgeKey;
 }
