f4ea790cd57e400bb9fbee3080c00e22029ebce5
jynnie <jynn.tang@gmail.com>
2022-12-29 14:52:13 -0800

    🏢 Remove popper

--- lib/juniper-hooks.ts
+++ lib/juniper-hooks.ts
@@ -1,50 +1,7 @@
 import { nanoid } from "nanoid";
-import { useMemo, useState } from "react";
-import { usePopper as useRawPopper } from "react-popper";
+import { useMemo } from "react";
 
 export function useUUID(size?: number) {
   const uuid = useMemo(() => nanoid(size), [size]);
   return uuid;
-}
-
-const SAME_WIDTH = {
-  name: "sameWidth",
-  enabled: true,
-  phase: "beforeWrite" as any,
-  requires: ["computeStyles"],
-  fn: ({ state }: any) => {
-    state.styles.popper.width = `${state.rects.reference.width}px`;
-  },
-  effect: ({ state }: any) => {
-    state.elements.popper.style.width = `${state.elements.reference.offsetWidth}px`;
-  },
-};
-
-const OFFSET = {
-  name: "offset",
-  options: {
-    offset: [0, 8],
-  },
-};
-
-export function usePopper() {
-  const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(
-    null,
-  );
-  const [popperElement, setPopperElement] = useState<HTMLElement | null>(null);
-  const { styles, attributes, forceUpdate } = useRawPopper(
-    referenceElement,
-    popperElement,
-    {
-      placement: "bottom-start",
-      modifiers: [OFFSET, SAME_WIDTH],
-    },
-  );
-  return {
-    setReferenceElement,
-    setPopperElement,
-    styles,
-    attributes,
-    update: forceUpdate,
-  };
 }
