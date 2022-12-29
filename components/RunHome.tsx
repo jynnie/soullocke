eee1335c065d3586c989429431a3b14c985aefb0
jynnie <jynn.tang@gmail.com>
2022-12-29 12:16:38 -0800

    🏢 New add location design

--- components/RunHome.tsx
+++ components/RunHome.tsx
@@ -1,13 +1,11 @@
-import { Tabs } from "antd";
+import classNames from "classnames";
 import Summary from "components/Summary";
 import Timeline from "components/Timeline";
 import { useGameName } from "hooks/useGameName";
 import { usePlayersArray } from "hooks/usePlayersArray";
 import type { MapLocation } from "models";
-import React from "react";
+import React, { useState } from "react";
 import styles from "styles/Run.module.scss";
-
-const { TabPane } = Tabs;
 
 interface Props {
   id: string;
@@ -19,20 +17,34 @@ function RunHome({ id, allBadges, allLocations }: Props) {
   const playerArr = usePlayersArray();
   const game = useGameName();
 
+  const [viewingTab, setViewingTab] = useState(0);
+
   return (
     <div className={styles.container}>
       <h2 className="capitalize">{game} Soullocke</h2>
       <h3>{playerArr.map((p) => p.name).join(" | ")}</h3>
       <div>codename: {id}</div>
 
-      <Tabs className={styles.tabs} defaultActiveKey="1" centered>
-        <TabPane tab="Timeline" key="1">
-          <Timeline {...{ allLocations, allBadges }} />
-        </TabPane>
-        <TabPane tab="Summary" key="2">
-          <Summary {...{ allBadges }} />
-        </TabPane>
-      </Tabs>
+      <div className="mt-8 mb-4 flex center gap-2">
+        {["Timeline", "Summary"].map((t, k) => (
+          <label
+            key={k}
+            className={classNames(styles.tab, {
+              [styles.selectedTab]: viewingTab === k,
+            })}
+            onClick={() => setViewingTab(k)}
+          >
+            {t}
+          </label>
+        ))}
+      </div>
+
+      <div className={classNames({ hidden: viewingTab !== 0 })}>
+        <Timeline {...{ allLocations, allBadges }} />
+      </div>
+      <div className={classNames({ hidden: viewingTab !== 1 })}>
+        <Summary {...{ allBadges }} key="Summary" />
+      </div>
     </div>
   );
 }
